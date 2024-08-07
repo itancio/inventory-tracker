0ce571371d8bc2b33dc3088bb01489c1c63aa346
Irvin Tancioco <irvin.tanc@gmail.com>
2024-08-06 19:06:47 -0700

    edit

--- app/page.js
+++ app/page.js
@@ -8,19 +8,19 @@ import { collection, doc, docs, getDocs, query, setDoc, deleteDoc, getDoc} from 
 import FilterableTable from './Components/FilterableTable';
 import Webcam from './Components/Webcam';
 
-const inventory = [
-  { id: 1,name: 'Apple', quantity: 5 },
-  { id: 2, name: 'Banana', quantity: 1 },
-  { id: 3, name: 'Orange', quantity: 3 },
-  { id: 4, name: 'Mango', quantity: 3 },
-  { id: 5, name: 'Pineapple', quantity: 2 },
-  { id: 6, name: 'Grapes', quantity: 4 },
-  { id: 7, name: 'Cherry', quantity: 1 },
-  { id: 8, name: 'Watermelon', quantity: 2 },
-  { id: 9, name: 'Pear', quantity: 4 },
-  { id: 10, name: 'Strawberry', quantity: 3 },
-  { id: 11, name: 'Lemon', quantity: 1 },
-];
+// const inventory = [
+//   { id: 1,name: 'Apple', quantity: 5 },
+//   { id: 2, name: 'Banana', quantity: 1 },
+//   { id: 3, name: 'Orange', quantity: 3 },
+//   { id: 4, name: 'Mango', quantity: 3 },
+//   { id: 5, name: 'Pineapple', quantity: 2 },
+//   { id: 6, name: 'Grapes', quantity: 4 },
+//   { id: 7, name: 'Cherry', quantity: 1 },
+//   { id: 8, name: 'Watermelon', quantity: 2 },
+//   { id: 9, name: 'Pear', quantity: 4 },
+//   { id: 10, name: 'Strawberry', quantity: 3 },
+//   { id: 11, name: 'Lemon', quantity: 1 },
+// ];
 
 
 
